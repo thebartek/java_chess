e765f29b671596ac4b3a2fa28129ecc413921bd2
thebartek <73034401+thebartek@users.noreply.github.com>
2020-12-07 19:11:27 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+package com.FREDDEV;
+
+import com.FREDDEV.Plansze.Plansza;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Plansza plansza = new Plansza();
+        plansza.Szachy();
+    }
+}
